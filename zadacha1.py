d819ccc5dfedafe1efd2f422f6787ab186a7bb7b
IaDobri <iatsan006@gmail.com>
2024-09-06 09:49:14 +0500

    Add files via upload

---
+++ zadacha1.py
@@ -0,0 +1,23 @@
+#1
+# b1 = int(input('введите 1 основание: '))
+# b2 = int(input('введите 2 основание: '))
+# h = int(input('введите высоту: '))
+# V = (b1 + b2) * h
+# S = V / 2
+# print(S)
+
+#2
+# a = input('введите 1 слово: ')
+# b = input('введите 2 слово: ')
+# c = input('введите 3 слово: ')
+# print(a, b, c)
+
+#3
+# from math import sqrt
+# x = int(input('введите число: '))
+# a = x // 100
+# b = (x//10)%10
+# c = x % 10
+# s = a + b + c
+# k = sqrt(s)
+# print(s, k)
