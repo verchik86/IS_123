ef8470599a1fa383e1ade25fbd9fd847524e0880
DAniL4628 <medalenovicmedalen@gmail.com>
2024-09-06 10:32:03 +0500

    Delete Линейные алгоритмы directory

--- Линейные алгоритмы/Задание 1.py
+++
@@ -1,7 +0,0 @@
-b1 = int(input('Введите b1 = '))
-b2 = int(input('Введите b2 = '))
-b3 = int(input('Введите h = '))
-
-S = ((b1 + b2) * b3)/2
-
-print('Площадь S = ', S)
--- Линейные алгоритмы/Задание 2.py
+++
@@ -1,7 +0,0 @@
-word1 = input("Введите первое слово: ")
-word2 = input("Введите второе слово: ")
-word3 = input("Введите третье слово: ")
-
-result = f"{word1} {word2} {word3}"
-
-print("Объединенные слова: ", result) 
