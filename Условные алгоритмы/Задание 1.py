1437859218d9213c8f1207b935c0811c27913f4d
DAniL4628 <medalenovicmedalen@gmail.com>
2024-09-06 10:32:20 +0500

    Delete Условные алгоритмы directory

--- Условные алгоритмы/Задание 1.py
+++
@@ -1,6 +0,0 @@
-a = int(input('Введите число a = ' ))
-b = int(input('Введите число b = ' ))
-if int(a >= b):
-    print('a больше b' )
-else:
-    print('b больше a' )
--- Условные алгоритмы/Задание 2.py
+++
@@ -1,7 +0,0 @@
-x1 = int(input('Введите число = '))
-if x1 >= 0:
-    y = 2 * x1 - 1
-else:
-    y = x1 ** 2
-
-print(y)
