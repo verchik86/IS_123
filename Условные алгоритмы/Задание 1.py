c5760937109d063205072a594be5e263cf149a76
DAniL4628 <medalenovicmedalen@gmail.com>
2024-09-06 10:13:42 +0500

    Add files via upload

---
+++ Условные алгоритмы/Задание 1.py
@@ -0,0 +1,6 @@
+a = int(input('Введите число a = ' ))
+b = int(input('Введите число b = ' ))
+if int(a >= b):
+    print('a больше b' )
+else:
+    print('b больше a' )
---
+++ Условные алгоритмы/Задание 2.py
@@ -0,0 +1,7 @@
+x1 = int(input('Введите число = '))
+if x1 >= 0:
+    y = 2 * x1 - 1
+else:
+    y = x1 ** 2
+
+print(y)
