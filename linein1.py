f59f9cb301f757f9a38bf671c929e7c170e9fcc6
Sovastud <oleinikov_slava@mail.ru>
2024-09-06 10:54:34 +0500

    Add files via upload

---
+++ linein1.py
@@ -0,0 +1,25 @@
+# B1 = float(input("Длинны первого основания "))
+# B2 = float(input("Длинна второго основания "))
+# H = float(input("Высота "))
+# N = B1 + B2 
+# K = N * H / 2
+# print("S =", K )
+
+# A = input("первое слово ")
+# B = input("второе слово ")
+# C = input("третье слово ")
+# print(A,B,C) 
+
+# from math import sqrt
+
+# X = int(input("Введите трехзначное число "))
+# if 100 <= X <= 999:
+#     A = X % 10
+#     B = X // 10
+#     C = B % 10
+#     D = B // 10
+#     K = A+C+D
+#     I = sqrt(K)
+#     print("S = ",K)
+#     print("K = ", round(I, 2))
+# print("неверное число")
