02fd8949c0d24451ec5ac8ea6d5d7a7879e2fd0e
DAniL4628 <medalenovicmedalen@gmail.com>
2024-09-06 10:24:13 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+#b1 = int(input('Введите b1 = '))
+#b2 = int(input('Введите b2 = '))
+#b3 = int(input('Введите h = '))
+
+#S = ((b1 + b2) * b3)/2
+
+#print('Площадь S = ', S)
+
+#word1 = input("Введите первое слово: ")
+#word2 = input("Введите второе слово: ")
+#word3 = input("Введите третье слово: ")
+
+#result = f"{word1} {word2} {word3}"
+
+#print("Объединенные слова: ", result) 
+
+#from math import sqrt
+#x = int(input('Введите число = '))
+#a = x % 10
+#one = x // 10
+#two = one % 10
+#three = one // 10
+#print(three, two, a, sep=' ')
+#S = int(three+two+a)
+#print('Сумма ',S)
+#print('Квадратный корень = ', sqrt(S))
