671e4fa949f38fa8d63096180fa89b70f9fdced1
Amplbek2281337 <toxtairill2804@mail.ru>
2024-09-06 10:41:33 +0500

    Add files via upload

---
+++ tohtamishev3.py
@@ -0,0 +1,21 @@
+# x = int(input('Введите начальную скорость - '))
+# y = int(input('Введите конечную скорость - '))
+# k = 1
+# while x < y:
+#     x = x * 1.1
+#     k = k + 1
+# else:
+#     print('Количество дней до конечной скорости - ', k)
+
+
+
+# x = int(input('Введите начальную сумму - '))
+# p = int(input('Введите процент увеличения суммы - '))
+# y = int(input('Введите целевая сумма вклада - '))
+# current_amount = x
+# years = 0
+# while current_amount < y:
+#     current_amount += current_amount * (p / 100)
+#     years += 1
+# else:
+#     print('Через', years, 'лет вклад составит не менее ',y, 'рублей')
