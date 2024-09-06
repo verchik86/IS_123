055cac12131f9160705589eeac9802386bbd1d5e
Viktorrik34488 <viktorkudasev9@gmail.com>
2024-09-06 10:57:05 +0500

    Add files via upload

---
+++ uslov1.py
@@ -0,0 +1,14 @@
+# A = input("Введите число ")
+# B = input("Введите число ")
+# if A > B:
+#     print("a больше б")
+# else:
+#     print("б больше а")
+
+# X = float(input("Введите число "))
+# if X >= 0:
+#     Y = 2 * X - 1
+#     print(Y)
+# else:
+#     Y = X ** 2 
+#     print(Y)
