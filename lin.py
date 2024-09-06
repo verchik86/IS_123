c7a9ec83b4b6a8635ff8e75dd026f6bd7219f31c
karinamenzhus <kmenki@bk.ru>
2024-09-06 09:34:01 +0500

    lin.py

---
+++ lin.py
@@ -0,0 +1,24 @@
+#1
+#b1 = int(input('b1 = '))
+#b2 = int(input('b2 = '))
+#h = int(input('h = '))
+#S = ((b1 + b2) * h)/2
+#print('S = ', S)
+
+#2
+#a = input('введите слово ')
+#b = input('введите слово ')
+#c = input('введите слово ')
+#d = ' '.join([a, b, c])
+#print(d)
+
+#3
+#from math import sqrt
+#import math
+#x = int(input('введите трёхзначное число '))
+#sto = int(x//100)
+#desyat = int((x//10) %10)
+#odin = int(x%10)
+#S = int(sto+desyat+odin)
+#koren = sqrt(S)
+#print('S = ', S
--- lin1.py
+++
@@ -1,7 +0,0 @@
-b1 = int(input('b1 = '))
-b2 = int(input('b2 = '))
-h = int(input('h = '))
-
-S = ((b1 + b2) * h)/2
-
-print('S = ', S)
