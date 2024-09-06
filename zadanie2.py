72a3f6f91eac2297942deb4b809a400b05c0fd69
IaDobri <iatsan006@gmail.com>
2024-09-06 10:10:52 +0500

    Add files via upload

---
+++ zadanie2.py
@@ -0,0 +1,16 @@
+#1
+# a = int(input('введите 1 число: '))
+# b = int(input('введите 2 число: '))
+# if a > b:
+#      print('a больше b')
+# else:
+#      print('b больше a')
+
+#2
+# x = int(input('введите число: '))
+# if x >= 0:
+#     y = 2 * x - 1
+#     print(y)
+# else:
+#     y = x ** 2
+#     print(y)
