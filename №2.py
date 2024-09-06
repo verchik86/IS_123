6b85897caa41d95eca348a1c25d05ba411ced73a
Lastimazar <samoteyas060607@gmail.com>
2024-09-06 10:04:27 +0500

    Add files via upload

---
+++ №2.py
@@ -0,0 +1,16 @@
+#№1
+#a = int(input('Введите число a - ' ))
+#b = int(input('Введите число b - ' ))
+#if int(a >= b):
+#    print('a больше b' )
+#else:
+#    print('b больше a' )
+
+#№2
+#x1 = int(input('Введите число - '))
+#if x1 >= 0:
+#    y = 2 * x1 - 1
+#else:
+#    y = x1 ** 2
+
+#print(y)
