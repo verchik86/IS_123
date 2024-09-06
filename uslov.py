694adc35407abd658f5e94bd73e52758ba78240a
karinamenzhus <kmenki@bk.ru>
2024-09-06 10:09:22 +0500

    uslov.py

---
+++ uslov.py
@@ -0,0 +1,15 @@
+#1
+# a = int(input('Введите число "a" '))
+# b = int(input('Введите второе число "b" '))
+# if a > b:
+#     print('a больше b')
+# else:
+#     print('b больше a')
+
+#2
+# x = int(input('Введите значение x '))
+# if x>=0:
+#     y = 2*x - 1 
+# else:
+#     y = x ** 2
+# print('y = ', y)
