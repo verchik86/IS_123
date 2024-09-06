ed6f2f7240f5343a1def0c124edfa8cb0b83664d
Amplbek2281337 <toxtairill2804@mail.ru>
2024-09-06 10:09:37 +0500

    Add files via upload

---
+++ Tohtamishev2.py
@@ -0,0 +1,15 @@
+#a = input('Введите число - ')
+#b = input('Введите число - ')
+#if a <= b:
+#    print('b больше a')
+#else:
+#    print('a больше b')
+
+
+#x = int(input('Введите число '))
+#if x >= 0:
+#    y = 2*x -1
+#    print('y = ',y)
+#else:
+#    y = x ** 2
+#    print('y = ',y)
