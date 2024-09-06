a715705089f220bd9bba54364e2fd0fc9371df19
Grunichev16 <Logan28004@yandex.ru>
2024-09-06 10:37:39 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+#a = int(input('Введите число a = ' ))
+#b = int(input('Введите число b = ' ))
+#if int(a >= b):
+#    print('a больше b' )
+#else:
+#    print('b больше a' )
+
+
+#x1 = int(input('Введите число = '))
+#if x1 >= 0:
+#    y = 2 * x1 - 1
+#else:
+#    y = x1 ** 2
+
+#print(y)
