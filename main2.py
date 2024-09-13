68b96e1164bdba67f541dac190ca97ed114652d5
ByFarra <f.kobulov@mail.ru>
2024-09-13 09:35:15 +0500

    Delete main2.py

--- main2.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Трехзначное число: "))
-a = int(x // 100)
-b = int((x//10)%10)
-c = int(x % 10)
-suma = a + b + c
-print("S = ", suma)
-k = suma**0.5
-num = round(k, 2)
-print("K = ", num)
