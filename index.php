c079a350bbfbbfa1edc0dddf768dd45aef2b27c0
DarkDantes <spark8645@yandex.ru>
2024-10-10 16:04:09 +0500

    Добавлен файл index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сергиенко test</title>
+    <br>
+    <br>
+    <style>
+.test{
+    color: red;
+}
+    </style>
+    
+<center>
+    <div class="test">
+<?='<h1><b>Hello World!</b></h1>'?>
+     </div>
+</center>
+</html>
